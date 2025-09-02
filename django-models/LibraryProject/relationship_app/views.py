7b78449a631136ee97da9f9f7ae21735423fd831
TaiWowAde <twin.z@hotmail.co.uk>
2025-09-02 16:53:19 +0100

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
-from django.contrib.auth import login, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import user_passes_test, permission_required
-from django.views.generic import DetailView
+from .models import Library
+from .models import Book
 
-from .models import Book, Library
-from .forms import BookForm
+from django.contrib.auth.decorators import permission_required
+
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.views.generic.detail import DetailView
+from .forms import BookForm 
 
 
 def list_books(request):
