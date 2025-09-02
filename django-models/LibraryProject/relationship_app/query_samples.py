21e7c866cd9e94450bb11dac17a325bec74cabc5
TaiWowAde <twin.z@hotmail.co.uk>
2025-09-02 15:37:57 +0100

    added library_name

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -17,8 +17,11 @@ for book in books_by_orwell:
     print(f"- {book.title}")
 
 # 2. List all books in a library
-print("\nBooks in Central Library:")
-for book in library.books.all():
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+books = library.books.all()
+print(f"\nBooks in {library_name}:")
+for book in books:
     print(f"- {book.title}")
 
 # 3. Retrieve the librarian for a library
